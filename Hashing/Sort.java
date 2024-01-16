29173ec298ef65575cd124b7c6c2c3269de6e684
Fahim Imtiaz <90858661+fahimquicklife8@users.noreply.github.com>
2024-01-15 22:32:41 -0600

    Add files via upload

---
+++ Hashing/Sort.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Sort{
+    public static void main(String[] args) {
+
+        int [] array =   {9,1,4,7,3,-1,0,5,8,-1,6};
+
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+}
