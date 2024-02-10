6989cf25eaefdf99c1ff1fb6382e4739d9b02839
Fahim Imtiaz <90858661+fahimquicklife8@users.noreply.github.com>
2024-02-09 23:10:41 -0600

    Add files via upload

---
+++ Hashing/Treeset.java
@@ -0,0 +1,20 @@
+import java.util.TreeSet;
+
+public class Treeset {
+    public static void main(String[] args) {
+        TreeSet<Integer> heap = new TreeSet<>();
+
+
+        heap.add(1);
+        heap.add(2);
+        heap.add(3);
+        heap.add(98);
+        heap.add(9);
+        heap.add(6);
+
+        System.out.println(heap);
+
+        }
+
+    }
+
